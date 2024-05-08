5c6a0c0ae36b01a59dc5265d821ea20e1c827098
Louay Badri <louay.badri@insat.ucat.tn>
2024-05-08 22:34:11 +0100

    hadoop ++ mongo

--- src/main/java/weather/batch/project/AvgTempPerCapital.java
+++ src/main/java/weather/batch/project/AvgTempPerCapital.java
@@ -23,3 +23,42 @@ public class AvgTempPerCapital {
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
+
+// package weather.batch.project;
+
+// import org.apache.hadoop.conf.Configuration;
+// import org.apache.hadoop.fs.Path;
+// import org.apache.hadoop.io.FloatWritable;
+// import org.apache.hadoop.io.Text;
+// import org.apache.hadoop.mapreduce.Job;
+// import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+// import com.mongodb.hadoop.MongoOutputFormat;
+// import java.net.URLEncoder;
+// import java.nio.charset.StandardCharsets;
+
+// // ...
+
+// public class AvgTempPerCapital {
+// public static void main(String[] args) throws Exception {
+// Configuration conf = new Configuration();
+
+// // Set MongoDB output URI
+// String encodedURL = URLEncoder.encode(
+// "mongodb+srv://admin:1234@cluster0.iunwbji.mongodb.net/bigdata.weather_average",
+// StandardCharsets.UTF_8.toString());
+
+// conf.set("mongo.output.uri", encodedURL);
+// Job job = Job.getInstance(conf, "average temperature per capital");
+// job.setJarByClass(AvgTempPerCapital.class);
+// job.setMapperClass(TokenizerMapper.class);
+// job.setReducerClass(AvgTempReducer.class);
+// job.setOutputKeyClass(Text.class);
+// job.setOutputValueClass(FloatWritable.class);
+// FileInputFormat.addInputPath(job, new Path(args[0]));
+
+// // Set MongoDB as the output format
+// job.setOutputFormatClass(MongoOutputFormat.class);
+
+// System.exit(job.waitForCompletion(true) ? 0 : 1);
+// }
+// }
--- src/main/java/weather/batch/project/AvgTempReducer.java
+++ src/main/java/weather/batch/project/AvgTempReducer.java
@@ -1,12 +1,21 @@
 package weather.batch.project;
 
+import org.bson.Document;
 import java.io.IOException;
 import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.UpdateOptions;
+
 public class AvgTempReducer extends Reducer<Text, FloatWritable, Text, FloatWritable> {
     private FloatWritable result = new FloatWritable();
+    private MongoClient mongoClient;
+    private MongoCollection<Document> collection;
 
     public void reduce(Text key, Iterable<FloatWritable> values, Context context)
             throws IOException, InterruptedException {
@@ -17,11 +26,35 @@ public class AvgTempReducer extends Reducer<Text, FloatWritable, Text, FloatWrit
             sum += val.get();
             count++;
         }
-        
+
         if (count > 0) {
             float avg = sum / count;
             result.set(avg);
             context.write(key, result);
         }
+
+        // Create a new document or update an existing one with the average temperature
+        Document filter = new Document("capital", key.toString());
+        Document update = new Document("$set", new Document("average_temperature", result.get()));
+        collection.updateOne(filter, update, new UpdateOptions().upsert(true));
     }
+
+    @Override
+    protected void setup(Context context) throws IOException, InterruptedException {
+        super.setup(context);
+        // Connect to the MongoDB server and get the collection
+        mongoClient = MongoClients.create("mongodb+srv://admin:1234@cluster0.iunwbji.mongodb.net");
+        MongoDatabase database = mongoClient.getDatabase("bigdata");
+        collection = database.getCollection("weather_average");
+        // TODO: Add any additional setup code here
+    }
+
+    @Override
+    protected void cleanup(Context context) throws IOException, InterruptedException {
+        super.cleanup(context);
+        // Close the MongoDB connection
+        mongoClient.close();
+        // TODO: Add any additional cleanup code here
+    }
+
 }
